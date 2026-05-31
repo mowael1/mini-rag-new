f7b4af9e5ad1b1add661ab50a64f775d13b62f4e
mowael1 <mw86596@gmail.com>
2026-05-31 23:26:16 +0300

    add generation & embedding models to main

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from src.routes import base,data
 from motor.motor_asyncio import AsyncIOMotorClient
 from src.helpers.config import get_settings
 from contextlib import asynccontextmanager
+from src.stores.llm.LLMProviderFactory import LLMProviderFactory
 
 settings = get_settings()
 
@@ -13,6 +14,16 @@ async def lifespan(app: FastAPI):
     app.mongo_client = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_client[settings.MONGODB_DATABASE]
     
+    LLM_provider_factory = LLMProviderFactory(config=settings)
+    
+    # generation client هنضيف ال 
+    app.generation_client = LLM_provider_factory.create(settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(settings.GENERATION_MODEL_ID)
+    
+    # embedding clientهنضيف ال 
+    app.embedding_client = LLM_provider_factory.create(settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(settings.EMBEDDING_MODEL_ID,
+                                            embedding_size=settings.EMBEDDING_MODEL_SIZE)  
     yield  # ← التطبيق شغال هنا
     
     # ← shutdown هنا
