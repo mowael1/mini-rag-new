23d08a560680db132360398d0e62650e61a09bf0
mowael1 <mw86596@gmail.com>
2026-05-31 23:26:50 +0300

    adding LLM factory

---
+++ src/stores/llm/LLMProviderFactory.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from stores.llm.LLMInterface import LLMInterface
+from stores.providers.OpenAIProvider import OpenAIProvider
+from stores.providers.CoHereProvider import CoHereProvider
+from llm.LLMEnums import LLMEnums
+
+class LLMProviderFactory:
+    
+    def __init__(self, config: dict):
+        self.config = config
+        
+        
+    def create(self, provider: str):
+        
+        if provider == LLMEnums.OPENAI.value:
+            return OpenAIProvider(
+                api_key = self.config.OPENAI_API_KEY,
+                api_url = self.config.OPENAI_API_URL,
+                default_input_max_char = self.config.default_input_max_char,
+                default_generation_max_output_tokens = self.config.default_generation_max_output_tokens,
+                defult_generation_temperature = self.config.defult_generation_temperature,
+            )
+        
+        if provider == LLMEnums.COHERE.value:
+            return CoHereProvider(
+                api_key = self.config.COHERE_API_KEY,
+                default_input_max_char = self.config.default_input_max_char,
+                default_generation_max_output_tokens = self.config.default_generation_max_output_tokens,
+                default_generation_temperature = self.config.default_generation_temperature
+            )
+        
+        raise ValueError(f"Unknown LLM provider: {provider}")
